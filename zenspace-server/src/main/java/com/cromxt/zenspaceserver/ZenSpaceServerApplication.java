ad9796861809f9f1e0e7009b1e0642d773d82a66
Akash Biswas <akashbiswas.271999@gmail.com>
2025-03-05 20:20:14 +0530

    Initialize the project.

---
+++ zenspace-server/src/main/java/com/cromxt/zenspaceserver/ZenSpaceServerApplication.java
@@ -0,0 +1,13 @@
+package com.cromxt.zenspaceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenSpaceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenSpaceServerApplication.class, args);
+	}
+
+}
