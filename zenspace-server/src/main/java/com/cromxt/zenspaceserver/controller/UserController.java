cd1ff2ffa665291c12f7d0b308ac2c5a107c2c56
Akash Biswas <akashbiswas.271999@gmail.com>
2025-03-06 09:30:32 +0530

    Chnage the name of the directory.

--- zenspace-server/src/main/java/com/cromxt/zenspaceserver/controller/UserController.java
+++ zenspace-server/src/main/java/com/cromxt/zenspaceserver/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.cromxt.zenspaceserver.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
+
+
+    @GetMapping
+    public Mono<String> greet(){
+        return Mono.just("Hello World!");
+    }
 }
