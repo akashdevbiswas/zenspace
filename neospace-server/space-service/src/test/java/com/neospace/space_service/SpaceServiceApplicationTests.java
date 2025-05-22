96e67ced13375ca174843cc073498608e1517d64
Akash Biswas <akashbiswas.271999@gmail.com>
2025-05-22 16:25:15 +0530

    Create space-service application.

---
+++ neospace-server/space-service/src/test/java/com/neospace/space_service/SpaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neospace.space_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
